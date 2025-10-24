7adc434242eb460bf41d9096f158ed5eb25fcf1a
mariavittoria27 <mariavittoriafiorucci@gmail.com>
2025-10-24 11:22:07 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        Cavallo fulmine = new Cavallo("Fulmine",50);
-        Cavallo lampo = new Cavallo( "Lampo", 10);
-        Cavallo rose = new Cavallo( "Rose", 990);
-        Cavallo flash = new Cavallo( "Flash", 1);
-        Cavallo rambo = new Cavallo( "Rambo", 138);
-
-        //flash.setPriority(Thread.MAX_PRIORITY);
-        //rose.setPriority(Thread.MIN_PRIORITY);
-
-
-        fulmine.start();
-        lampo.start();
-        rose.start();
-        flash.start();
-        rambo.start();
-    }
+static String primo=';
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+String tmpS;
+int tmp;
+ArrayList<Cavallo> listaCavallo = new ArrayList<Cavallo>(;
+for (int i = 1; 1 <= 4; i++) {
+System.out.println("Inserisci il nome del cavallo " + i);
+tmpS = input.nextLine();
+System.out.println("Inserisci la lentezza del cavallo " + i);
+tmp = input.nextInt();
+String v = input.nextLine(); //prende il \n
+Cavallo c=new Cavallo(tmpS, tmp);
+listaCavallo.add(c);
+for(Cavallo c: listaCavallo{
+c.start();
+｝
+int n = (int) (Math.random () * (listaCavallo.size() +1));
+Cavallo x = listaCavallo.get(n);
+x.interrupt();
+    113
+System.out.println("Il cavallo interrotto è: " + x.getName());
+for(Cavallo c: listaCavallo{
+try {
+c.join();
+} catch (InterruptedException e) {
+interrotto = true;
+System.out.println(name + " è stato interrotto!");
+return;
+System.out.println("Il primo cavallo:" + primo); }
+public static String getPrimo () {
+return primo;
+｝
+public static void setPrimo(String primo) {
+Main.primo = primo;
 }
+}
+    
