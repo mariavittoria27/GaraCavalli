29d37f98f0812d35e059cfd10e018c29ace74930
mariavittoria27 <mariavittoriafiorucci@gmail.com>
2025-10-24 13:25:09 +0200

    Cavallo.java

--- src/Cavallo.java
+++ src/Cavallo.java
@@ -1,33 +1,40 @@
-/*questa classe è il nostro primo thread e consentirà la simulazione di un cavallo in corsa parallelamente con altri della stessa specie
-* @author fiorucci * @version 1.0
-* */
 public class Cavallo extends Thread{
-private final String name;
-private int lentezza;
-private boolean interrotto = false;
-public Cavallo(String name, int lentezza) { super();
-this.name = name;
-this.lentezza = lentezza; }
-/**override del metodo run */ @Override
-public void run(){
-System.out.println("Cavallo " +
-name + " comincia il suo galoppo");
-for (int i = 1; i <= 10 && !Thread.currentThread().isInterrupted(); i++) { 
-    try {
-sleep(lentezza);
-} catch (InterruptedException e) {
-interrotto = true;
-System.out.println(name + " è stato interrotto!"); return;
-}
-System.out.println(name +" cavalca - passo: " + i); }
-if(Main.getPrimo().equals("") && !interrotto){
-    Main.setPrimo(this.name);
-} }
-/**getter per il parametro di lentezza SUPERFLUI*/
-    protected int getLentezza(){
-return lentezza; }
-/**setter per il parametro di lentezza */ 
-    protected void setLentezza(int lentezza){
-  this.lentezza=lentezza;
 
- } }
+    private final String name;
+    private int lentezza;
+
+    public Cavallo(String name, int lentezza) {
+        super();
+        this.name = name;
+        this.lentezza = lentezza;
+    }
+    /**override del metodo run */
+    @Override
+    public void run(){
+        System.out.println("Cavallo " +
+                name + " comincia il suo galoppo");
+        for (int i = 1; i <= 10 && !Thread.currentThread().isInterrupted(); i++) {
+            try {
+                sleep(lentezza);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(name +" cavalca - passo: " + i);
+        }
+        if(Main.getPrimo().equals("") && !Thread.currentThread().isInterrupted()){
+            Main.setPrimo(this.name);
+        }
+    }
+    /**getter per il parametro di lentezza SUPERFLUI*/
+    protected int getLentezza(){
+        return lentezza;
+    }
+    /**setter per il parametro di lentezza */
+    protected void setLentezza(int lentezza){
+        this.lentezza=lentezza;
+    }
+
+    public String getNomeCavallo(){
+        return name;
+    }
+}
